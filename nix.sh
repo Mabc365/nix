5de68a923d1b3f6a9b0e029813a952103e9caeaf
Mabc365 <85178607+Mabc365@users.noreply.github.com>
2024-05-05 15:38:53 -0400

    Update nix.sh

--- nix.sh
+++ nix.sh
@@ -1,1 +1,5 @@
-sh <(curl -L https://nixos.org/nix/install) --daemon
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+ . ~/.nix-profile/etc/profile.d/nix.sh
+export NIX_CONFIG="experimental-features = nix-command flakes"
+
+nix profile install github:Aylur/dotfiles
